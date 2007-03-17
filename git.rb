b98d312d9ab832dddb14d8fd7ac242963bf6a450
Tilman Sauerbeck <tilman@code-monkey.de>
2007-03-17 23:39:54 +0100

    Make Git#initialize easier to read.

--- git.rb
+++ git.rb
@@ -2,7 +2,8 @@ class Git
 	attr_accessor(:git_dir)
 
 	def initialize(git_bin, git_dir)
-		@git_bin, @git_dir = git_bin, File.expand_path(git_dir)
+		@git_bin = git_bin
+		@git_dir = File.expand_path(git_dir)
 	end
 
 	def method_missing(cmd, *opts)
